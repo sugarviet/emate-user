41f772c375a154aa338f4a44267614704dc6d93d
Kim Loan <kimlloan146@gmail.com>
2023-09-10 08:12:21 +0700

    Update page.jsx

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,6 +1,12 @@
 import Cart from '@/components/Cart'
 import React from 'react'
 
+export const metadata = {
+  title: "Emate - Cart",
+  description: "Emate cart",
+  keywords: "cart, emate cart, online learning"
+};
+
 const CartPage = () => {
   return (
     <>
