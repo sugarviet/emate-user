b15848e82cbbb522dda3700ca18b25ece70db13f
nobi-onway <115331625+nobi-onway@users.noreply.github.com>
2023-10-10 16:09:29 +0700

    finish

--- src/components/CourseDetail/CourseReview/index.jsx
+++ src/components/CourseDetail/CourseReview/index.jsx
@@ -2,15 +2,29 @@ import { Avatar, Rate } from "antd";
 import { StarFilled } from "@ant-design/icons";
 import useSWR from "swr";
 import { get_fetcher } from "@/utils/fetcher";
-import { course_all_reviews_api, course_rating_api } from "@/constants/api";
+import { course_reviews_api } from "@/constants/api";
+import { useEffect, useState } from "react";
 
 function CourseReview({ id, average_rating, top_reviews }) {
-  const { data: reviews, isLoading: reviewLoading } = useSWR(
-    course_all_reviews_api(id),
+  const [currentReviewPage, setCurrentReviewPage] = useState(1);
+  const [reviews, setReviews] = useState(top_reviews);
+  const [loadedReviews, setLoadedReviews] = useState([]);
+
+  const { data: reviewsData, isLoading: reviewLoading } = useSWR(
+    course_reviews_api(id, currentReviewPage),
     get_fetcher
   );
 
-  if (reviewLoading) return null;
+  const handleLoadMoreComments = () => {
+    setReviews([...reviews, ...loadedReviews]);
+    setCurrentReviewPage(currentReviewPage + 1);
+  };
+
+  useEffect(() => {
+    if (reviewLoading || !reviewsData) return;
+
+    setLoadedReviews(reviewsData);
+  }, [reviewLoading, reviewsData, currentReviewPage]);
 
   return (
     <div>
@@ -19,18 +33,29 @@ function CourseReview({ id, average_rating, top_reviews }) {
         rating
       </span>
       <div className="grid grid-rows-1 md:grid-rows-none grid-flow-col md:grid-flow-row overflow-scroll md:overflow-hidden md:grid-cols-2 gap-2 md:gap-4 mt-4">
-        {top_reviews.map((review, index) => (
-          <div key={`${review} + ${index}`} className="border-t-2 py-4">
-            <div className="flex items-center my-2">
-              <Avatar />
-              <div className="flex-col flex text-sm ml-2">
-                <span>{review.user}</span>
-                <Rate value={5} disabled className="text-xs" />
+        {reviews.map((review, index) => {
+          const { name, avatar } = review.user;
+          return (
+            <div key={`${review} + ${index}`} className="border-t-2 py-4">
+              <div className="flex items-center my-2">
+                <Avatar src={avatar} />
+                <div className="flex-col flex text-sm ml-2">
+                  <span>{name}</span>
+                  <Rate value={review.rating} disabled className="text-xs" />
+                </div>
               </div>
+              <span>{review.comment}</span>
             </div>
-            <span>{review.comment}</span>
-          </div>
-        ))}
+          );
+        })}
+      </div>
+      <div className="w-full flex items-center justify-center">
+        <button
+          onClick={handleLoadMoreComments}
+          className="w-24 h-12 bg-pink-300 text-white my-8 font-bold"
+        >
+          Xem thêm
+        </button>
       </div>
     </div>
   );
