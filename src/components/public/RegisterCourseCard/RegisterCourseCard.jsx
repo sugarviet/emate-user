8801985f1bb50e266cc08d0866bb1a31ced4dfa7
nobi-onway <115331625+nobi-onway@users.noreply.github.com>
2023-10-11 11:22:30 +0700

    finish

--- src/components/public/RegisterCourseCard/RegisterCourseCard.jsx
+++ src/components/public/RegisterCourseCard/RegisterCourseCard.jsx
@@ -1,32 +1,34 @@
-"use client"
+"use client";
 import { motion as m } from "framer-motion";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 
 import Image from "next/image";
 
 import { Rate } from "antd";
+import { formattedCoin } from "@/utils/formatedCurrency";
 
-const RegisterCourseCard = ({cardData}) => {
-  const {id, index, thumbnail, courseTitle, author, rate, numberOfRates, price} = cardData;
+const RegisterCourseCard = ({ cardData }) => {
+  const { id, index, image, name, rating, purchasers, owner, price } = cardData;
   return (
     <div className="flex flex-col items-center py-3 w-80">
       {/* lg:w-80 md:w-80 xl:w-80 flex flex-col items-center py-3 w-64 */}
       <div>
-        <Image src={thumbnail} alt="img" height={250} width={250} />
+        <Image src={image} alt="img" height={250} width={250} />
       </div>
       <div className="px-2 md:px-0 w-64">
         <div className="h-14 w-full truncate_2_lines mt-2">
-          <h1 className="text-lg font-bold">{courseTitle}</h1>
+          <h1 className="text-lg font-bold">{name}</h1>
         </div>
-        <p className="text-gray-500 truncate_2_lines">
-          {author}
-        </p>
+        <p className="text-gray-500 truncate_2_lines">{owner?.name}</p>
         <div className="flex items-center gap-4">
-          <Rate value={rate} />
-          <p className="text-gray-500">({numberOfRates})</p>
+          <Rate disabled value={rating} />
+          <p className="text-gray-500">({purchasers})</p>
         </div>
-        <p className="text-lg font-bold">đ{price.toFixed(3)}</p>
-        <m.span className="float-right cursor-pointer" whileHover={{scale: 1.4}}>
+        <p className="text-lg font-bold">{formattedCoin(price)}</p>
+        <m.span
+          className="float-right cursor-pointer"
+          whileHover={{ scale: 1.4 }}
+        >
           <ShoppingCartOutlined className="text-3xl" />
         </m.span>
       </div>
