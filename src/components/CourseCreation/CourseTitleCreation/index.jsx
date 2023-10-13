d115b599cb784fbe5a5146be042cda9635851f15
nobi-onway <115331625+nobi-onway@users.noreply.github.com>
2023-10-13 20:16:23 +0700

    finish

--- src/components/CourseCreation/CourseTitleCreation/index.jsx
+++ src/components/CourseCreation/CourseTitleCreation/index.jsx
@@ -1,7 +1,8 @@
 import { Input } from "antd";
+import { useEffect, useState } from "react";
 import { Controller } from "react-hook-form";
 
-function CourseTitleCreation({ control }) {
+function CourseTitleCreation({ control, course }) {
   return (
     <div className="flex flex-col items-center">
       <span className="font-bold text-3xl mb-4">
@@ -13,6 +14,7 @@ function CourseTitleCreation({ control }) {
       <Controller
         name="name"
         control={control}
+        defaultValue={course.name}
         render={({ field }) => (
           <Input
             className="my-4 w-3/5 text-lg h-16"
--- src/components/CourseCreation/index.jsx
+++ src/components/CourseCreation/index.jsx
@@ -3,14 +3,20 @@ import { Steps, message } from "antd";
 
 import { motion as m } from "framer-motion";
 import styles from "./CourseCreation.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CourseTitleCreation from "./CourseTitleCreation";
 import CourseCategoryCreation from "./CourseCategoryCreation";
 import CourseDetailCreation from "./CourseDetailCreation";
 import { useForm } from "react-hook-form";
-import { post_fetcher } from "@/utils/fetcher";
-import { COURSE_API } from "@/constants/api";
-import { useRouter } from "next/navigation";
+import { get_fetcher, post_fetcher, put_fetcher } from "@/utils/fetcher";
+import {
+  COURSE_API,
+  course_item_api,
+  course_update_api,
+} from "@/constants/api";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+import { useChatStore } from "@/stores/useChatStore";
 
 const DEFAULT_STATE = {
   title: "Course Title",
@@ -46,6 +52,27 @@ const steps = [
 function CourseCreation() {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
   const { control, handleSubmit } = useForm();
+  const [course, setCourse] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { currentUserInfo } = useChatStore();
+  const { _id } = currentUserInfo;
+
+  const params = useSearchParams();
+  const id = params.get("id");
+
+  useEffect(async () => {
+    if (!id) return;
+
+    setIsLoading(true);
+
+    await axios
+      .get(course_item_api(id))
+      .then((res) => res.data)
+      .then((res) => res.metaData)
+      .then((data) => setCourse(data))
+      .then(() => setIsLoading(false));
+  }, []);
 
   const router = useRouter();
 
@@ -60,7 +87,7 @@ function CourseCreation() {
   const handleCreateCourse = async (data) => {
     const newCourse = {
       ...data,
-      owner: "651e2891262f21a98b85eb9e",
+      owner: _id,
       whatWillLearn: data.whatWillLearn.map((item) => item.value),
       content: data.content.map((item) => item.value),
     };
@@ -78,13 +105,41 @@ function CourseCreation() {
     );
   };
 
+  const handleUpdateCourse = async (data) => {
+    const newCourse = {
+      ...data,
+      name: data.name,
+      whatWillLearn: data.whatWillLearn.map((item) => item.value),
+      content: data.content.map((item) => item.value),
+    };
+
+    await put_fetcher(
+      course_update_api(id),
+      newCourse,
+      () => {
+        message.success("Bạn đã cập nhật khóa học thành công");
+        router.push("/instructor/courses");
+      },
+      (err) => {
+        message.error("Bạn đã cập nhật khóa học thất bại");
+      }
+    );
+  };
+
   const Form = steps[currentStepIndex].component;
 
+  if (isLoading) return;
+
   return (
-    <m.div className={styles.blur_bg} initial={{opacity: 0, x: -100}} animate={{x: 1, opacity: 1}} transition={{duration: 0.4}}>
+    <m.div
+      className={styles.blur_bg}
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ x: 1, opacity: 1 }}
+      transition={{ duration: 0.4 }}
+    >
       <Steps progressDot current={currentStepIndex} items={steps} />
       <div className="my-12 flex-1">
-        <Form control={control} />
+        <Form control={control} course={course} />
       </div>
       <div className="flex w-full h-32 justify-between">
         {steps[currentStepIndex].isPrevious ? (
@@ -103,6 +158,13 @@ function CourseCreation() {
             className="bg-pink-300 w-40 h-16 text-white font-bold text-xl"
           >
             Bước tiếp
+          </button>
+        ) : id ? (
+          <button
+            onClick={handleSubmit(handleUpdateCourse)}
+            className="bg-pink-300 w-48 h-16 text-white font-bold text-xl"
+          >
+            Cập nhật khóa học
           </button>
         ) : (
           <button
