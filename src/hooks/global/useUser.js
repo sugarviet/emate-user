a5e59df6721d052b5a61ebc72222ce5a38a05aa9
nobi-onway <115331625+nobi-onway@users.noreply.github.com>
2023-10-19 13:44:53 +0700

    added role management

--- src/hooks/global/useUser.js
+++ src/hooks/global/useUser.js
@@ -8,20 +8,19 @@ import { useEffect, useState } from "react";
 function useUser() {
     const { currentUserInfo } = useChatStore();
     const { get_with_header_fetcher } = useFetcher()
-    const [currentRoleIndex, setCurrentRoleIndex] = useState(0)
-    const [currentRole, setCurrentRole] = useState({})
+    const [roles, setRoles] = useState([]);
 
     const { data: purchasedCourses } = useSWR(PURCHASED_COURSE_API, get_with_header_fetcher)
     const { data: user } = useSWR(user_api(currentUserInfo._id), get_fetcher)
 
-    const switchRole = () => { setCurrentRoleIndex(currentRoleIndex ? 0 : 1) }
-
     useEffect(() => {
         if (!user) return;
-        setCurrentRole(user.role[currentRoleIndex])
-    }, [user, currentRoleIndex])
 
-    return { purchasedCourses, user, currentRole, switchRole };
+        const userRoles = user.role.map(role => role.name);
+        setRoles(userRoles)
+    }, [user])
+
+    return { purchasedCourses, user, roles };
 }
 
 export default useUser;
